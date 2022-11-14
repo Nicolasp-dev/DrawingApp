465e1c2fbba7c77a958dd806d845a07d8e3bc064
nicolaspc-dev <nicolaspc.wd@gmail.com>
2022-11-14 13:50:50 -0500

    Line persistence on the View

--- app/src/main/java/com/kotlin/drawingapp/DrawingView.kt
+++ app/src/main/java/com/kotlin/drawingapp/DrawingView.kt
@@ -12,10 +12,10 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
   private var mCanvasBitmap: Bitmap? = null
   private var mDrawPaint: Paint? = null
   private var mCanvasPaint: Paint? = null
-  private var canvas : Canvas? = null
+  private var canvas: Canvas? = null
   private var mBrushSize: Float = 0.toFloat()
   private var color = Color.BLACK
-
+  private val mPaths = ArrayList<CustomPath>()
 
   init{
     setUpDrawing()
@@ -42,6 +42,13 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
   override fun onDraw(canvas: Canvas) {
     super.onDraw(canvas)
     canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+    for(path in mPaths){
+      mDrawPaint!!.strokeWidth = path.brushThickness
+      mDrawPaint!!.color = path.color
+      canvas.drawPath(path, mDrawPaint!!)
+    }
+
     if(!mDrawPath!!.isEmpty){
       mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
       mDrawPaint!!.color = mDrawPath!!.color
@@ -64,6 +71,7 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
         mDrawPath!!.lineTo(touchX!!, touchY!!)
       }
       MotionEvent.ACTION_UP -> {
+        mPaths.add(mDrawPath!!)
         mDrawPath = CustomPath(color, mBrushSize)
       }
       else -> return false
