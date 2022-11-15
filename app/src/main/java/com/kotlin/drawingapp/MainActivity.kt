43c9caf1eff350fcefbcef4268aadb0f00740bf6
nicolaspc-dev <nicolaspc.wd@gmail.com>
2022-11-15 17:30:51 -0500

    Adding Gallery ImageButton

--- app/src/main/java/com/kotlin/drawingapp/MainActivity.kt
+++ app/src/main/java/com/kotlin/drawingapp/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -18,10 +17,12 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
     drawingView = findViewById(R.id.drawing_view)
-    drawingView?.setSizeForBrush(20.toFloat())
+    drawingView?.setSizeForBrush(5.toFloat())
 
     val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
     mImageButtonCurrentPaint = linearLayoutPaintColors[3] as ImageButton
     mImageButtonCurrentPaint!!.setImageDrawable(
       ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
@@ -35,24 +36,27 @@ class MainActivity : AppCompatActivity() {
 
   private fun showBrushSizeChooserDialog(){
     val brushDialog = Dialog(this)
+
     brushDialog.setContentView(R.layout.dialog_brush_size)
     brushDialog.setTitle("Brush size :")
+
     val smallBtn : ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
     smallBtn.setOnClickListener {
-      drawingView?.setSizeForBrush(10.toFloat())
-      brushDialog.dismiss()
-    }
-    val mediumBtn : ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
-    mediumBtn.setOnClickListener {
-      drawingView?.setSizeForBrush(20.toFloat())
-      brushDialog.dismiss()
-    }
-    val largeBtn : ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
-    largeBtn.setOnClickListener {
-      drawingView?.setSizeForBrush(30.toFloat())
+      drawingView?.setSizeForBrush(2.toFloat())
       brushDialog.dismiss()
     }
 
+    val mediumBtn : ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+    mediumBtn.setOnClickListener {
+      drawingView?.setSizeForBrush(5.toFloat())
+      brushDialog.dismiss()
+    }
+
+    val largeBtn : ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+    largeBtn.setOnClickListener {
+      drawingView?.setSizeForBrush(10.toFloat())
+      brushDialog.dismiss()
+    }
     brushDialog.show()
   }
 
